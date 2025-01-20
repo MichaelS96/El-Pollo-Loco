35c78eb10b9d7fe4c42bb2c7a3f824fe34543c0d
MichaelS96 <m.schwittay96@web.de>
2025-01-20 17:04:25 +0100

    class added and first objects added

--- js/game.js
+++ js/game.js
@@ -1,14 +1,10 @@
 let canvas;
 let ctx;
-let character = new Image();
+let world = new World();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-
-    character.src = '../img/2_character_pepe/1_idle/idle/I-1.png'
-
-
-    ctx.drawImage(character, 20, 20, 50, 150); 
+console.log('My Character is', world.character)
 }
