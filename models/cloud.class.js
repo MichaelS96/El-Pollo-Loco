327c25c46d3e123223745bad6d0c4abedd865911
MichaelS96 <m.schwittay96@web.de>
2025-01-21 09:31:47 +0100

    clouds added with random spawn

---
+++ models/cloud.class.js
@@ -0,0 +1,11 @@
+class Cloud extends MovableObject {
+    y = 25;
+    height = 250;
+    width = 450;
+
+    constructor() {
+        super().loadImage('img/5_background/layers/4_clouds/1.png');
+
+        this.x = Math.random() * 500; //spawnt die chicken zwischen 200 und 700
+    }
+}
