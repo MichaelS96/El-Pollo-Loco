06e5b20e05c41e11dcd703354ee0b79a60562b84
MichaelS96 <m.schwittay96@web.de>
2025-01-21 15:09:33 +0100

    cloud animation to left

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -5,7 +5,13 @@ class Cloud extends MovableObject {
 
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
+        this.x = Math.random() * 500;
+        this.animation();
+    }
 
-        this.x = Math.random() * 500; 
+    animation() {
+        setInterval(() => {
+            this.x -= 0.25;
+        }, 1000 / 60);
     }
 }
