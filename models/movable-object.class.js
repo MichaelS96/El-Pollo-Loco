ec2a21418efff152b6b6ed788d7db6461183b826
MichaelS96 <m.schwittay96@web.de>
2025-01-22 08:58:12 +0100

    imageCache for character

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,18 +4,26 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+
     moveRight() {
         console.log('Moving Right');
-
     }
 
     moveLeft() {
-
+        console.log('Moving Left');
     }
 }
