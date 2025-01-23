7563f62a128896e424e6d13b0beeba41184a33c0
MichaelS96 <m.schwittay96@web.de>
2025-01-23 09:27:55 +0100

    pepe jump added/keycode for up and down change it was swapped

--- js/game.js
+++ js/game.js
@@ -17,15 +17,15 @@ window.addEventListener("keydown", (e) => {
     if (e.keyCode == 37) {
         keyboard.LEFT = true;
     }
-    if (e.keyCode == 40) {
+    if (e.keyCode == 38) {
         keyboard.UP = true;
     }
-    if (e.keyCode == 38) {
+    if (e.keyCode == 40) {
         keyboard.DOWN = true;
     }
     if (e.keyCode == 32) {
         keyboard.SPACE = true;
-    }
+    }    
 });
 
 window.addEventListener("keyup", (e) => {
@@ -35,10 +35,10 @@ window.addEventListener("keyup", (e) => {
     if (e.keyCode == 37) {
         keyboard.LEFT = false;
     }
-    if (e.keyCode == 40) {
+    if (e.keyCode == 38) {
         keyboard.UP = false;
     }
-    if (e.keyCode == 38) {
+    if (e.keyCode == 40) {
         keyboard.DOWN = false;
     }
     if (e.keyCode == 32) {
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -13,7 +13,7 @@ class MovableObject {
 
     applyGravity() {
         setInterval(() => {
-            if (this.isAboveGround()) {
+            if (this.isAboveGround() || this.speedY > 0) {
                 this.y -= this.speedY;
                 this.speedY -= this.acceleration;
             }
@@ -21,7 +21,7 @@ class MovableObject {
     }
 
     isAboveGround() {
-        return this.y < 150;
+        return this.y < 140;
     }
 
     loadImage(path) {
