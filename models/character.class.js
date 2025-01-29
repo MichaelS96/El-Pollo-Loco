82b4a80c11c6f5503c569ffa7178565479c2bedd
MichaelS96 <m.schwittay96@web.de>
2025-01-29 12:44:09 +0100

    standing and sleeping animation added

--- models/character.class.js
+++ models/character.class.js
@@ -4,14 +4,45 @@ class Character extends MovableObject {
     width = 150;
     y = 60;
     speed = 5;
+    standingTime = 0;
+    world;
+    walking_sound = new Audio('audio/sand_walking.mp3');
+
+    IMAGES_STANDING = [
+        'img/2_character_pepe/1_idle/idle/I-1.png',
+        'img/2_character_pepe/1_idle/idle/I-2.png',
+        'img/2_character_pepe/1_idle/idle/I-3.png',
+        'img/2_character_pepe/1_idle/idle/I-4.png',
+        'img/2_character_pepe/1_idle/idle/I-5.png',
+        'img/2_character_pepe/1_idle/idle/I-6.png',
+        'img/2_character_pepe/1_idle/idle/I-7.png',
+        'img/2_character_pepe/1_idle/idle/I-8.png',
+        'img/2_character_pepe/1_idle/idle/I-9.png',
+        'img/2_character_pepe/1_idle/idle/I-10.png',
+    ];
+
+    IMAGES_SLEEPING = [
+        'img/2_character_pepe/1_idle/long_idle/I-11.png',
+        'img/2_character_pepe/1_idle/long_idle/I-12.png',
+        'img/2_character_pepe/1_idle/long_idle/I-13.png',
+        'img/2_character_pepe/1_idle/long_idle/I-14.png',
+        'img/2_character_pepe/1_idle/long_idle/I-15.png',
+        'img/2_character_pepe/1_idle/long_idle/I-16.png',
+        'img/2_character_pepe/1_idle/long_idle/I-17.png',
+        'img/2_character_pepe/1_idle/long_idle/I-18.png',
+        'img/2_character_pepe/1_idle/long_idle/I-19.png',
+        'img/2_character_pepe/1_idle/long_idle/I-20.png',
+    ];
+
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
         'img/2_character_pepe/2_walk/W-23.png',
         'img/2_character_pepe/2_walk/W-24.png',
         'img/2_character_pepe/2_walk/W-25.png',
-        'img/2_character_pepe/2_walk/W-26.png'
+        'img/2_character_pepe/2_walk/W-26.png',
     ];
+
     IMAGES_JUMPING = [
         'img/2_character_pepe/3_jump/J-31.png',
         'img/2_character_pepe/3_jump/J-32.png',
@@ -23,11 +54,13 @@ class Character extends MovableObject {
         'img/2_character_pepe/3_jump/J-38.png',
         'img/2_character_pepe/3_jump/J-39.png',
     ];
+
     IMAGES_HURT = [
         'img/2_character_pepe/4_hurt/H-41.png',
         'img/2_character_pepe/4_hurt/H-42.png',
         'img/2_character_pepe/4_hurt/H-43.png',
     ];
+
     IMAGES_DEAD = [
         'img/2_character_pepe/5_dead/D-51.png',
         'img/2_character_pepe/5_dead/D-52.png',
@@ -37,14 +70,11 @@ class Character extends MovableObject {
         'img/2_character_pepe/5_dead/D-56.png',
         'img/2_character_pepe/5_dead/D-57.png',
     ];
-    currentImage = 0;
-    world;
-    walking_sound = new Audio('audio/sand_walking.mp3');
-
-
 
     constructor() {
-        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
+        this.loadImages(this.IMAGES_STANDING);
+        this.loadImages(this.IMAGES_SLEEPING);
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_JUMPING);
         this.loadImages(this.IMAGES_DEAD);
@@ -54,46 +84,66 @@ class Character extends MovableObject {
     }
 
     animate() {
-
         setInterval(() => {
-            this.walking_sound.pause();
-            this.walking_sound.volume = 0.05;
-            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.moveRight();
-                this.otherDirection = false;
-                this.walking_sound.play();
-            }
-            if (this.world.keyboard.LEFT && this.x > 0) {
-                this.moveLeft();
-                this.otherDirection = true;
-                this.walking_sound.play();
-            }
-
-            if (this.world.keyboard.UP && !this.isAboveGround()) {
-                this.jump();
-            }
-
-            this.world.camera_x = -this.x + 100;
+            this.characterMoving();
         }, 1000 / 60);
 
         setInterval(() => {
+            this.characterAnimation();
+        }, 100);
+    }
 
-            if (this.isDead()) {
-                this.playAnimation(this.IMAGES_DEAD);
-            } else if (this.itHurt()) {
-                this.playAnimation(this.IMAGES_HURT)
-            } else if (this.isAboveGround()) {
-                this.playAnimation(this.IMAGES_JUMPING);
+    characterMoving() {
+        this.walking_sound.pause();
+        this.walking_sound.volume = 0.05;
+
+        if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+            this.moveRight();
+            this.otherDirection = false;
+            this.walking_sound.play();
+            this.standingTime = 0;
+        }
+        if (this.world.keyboard.LEFT && this.x > 0) {
+            this.moveLeft();
+            this.otherDirection = true;
+            this.walking_sound.play();
+            this.standingTime = 0;
+        }
+        if (this.world.keyboard.UP && !this.isAboveGround()) {
+            this.jump();
+            this.standingTime = 0;
+        }
+
+        this.world.camera_x = -this.x + 120;
+    }
+
+    characterAnimation() {
+        if (this.isDead()) {
+            this.playAnimation(this.IMAGES_DEAD);
+        } else if (this.itHurt()) {
+            this.playAnimation(this.IMAGES_HURT);
+            this.standingTime = 0;
+        } else if (this.isAboveGround()) {
+            this.playAnimation(this.IMAGES_JUMPING);
+            this.standingTime = 0;
+        } else {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                this.playAnimation(this.IMAGES_WALKING);
+                this.standingTime = 0;
             } else {
-                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                    //lauf animation
-                    this.playAnimation(this.IMAGES_WALKING);
+                this.playAnimation(this.IMAGES_STANDING);
+                this.standingTime += 300;
+
+                if (this.standingTime >= 10000) {
+                    this.playAnimation(this.IMAGES_SLEEPING);
                 }
             }
-        }, 100);
+        }
     }
 
     jump() {
         this.speedY = 30;
     }
+
+
 }
