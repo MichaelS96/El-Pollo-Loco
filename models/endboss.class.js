6afc588cb8550c64f8d891def215c5130a687ad2
MichaelS96 <m.schwittay96@web.de>
2025-01-30 14:40:20 +0100

    endboss IMAGES added/offset added

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,11 +1,15 @@
 class Endboss extends MovableObject {
-
     height = 400;
     width = 250;
     y = 50;
-    
+    offset = {
+        top: 0,
+        bottom: 50,
+        right: 0,
+        left: 0,
+    }
 
-    IMAGES_WALKING = [
+    IMAGES_ALERT = [
         'img/4_enemie_boss_chicken/2_alert/G5.png',
         'img/4_enemie_boss_chicken/2_alert/G6.png',
         'img/4_enemie_boss_chicken/2_alert/G7.png',
@@ -15,17 +19,48 @@ class Endboss extends MovableObject {
         'img/4_enemie_boss_chicken/2_alert/G11.png',
         'img/4_enemie_boss_chicken/2_alert/G12.png',
     ];
+    IMAGES_WALK = [
+        'img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img/4_enemie_boss_chicken/1_walk/G4.png',
+    ];
+    IMAGES_ATTACK = [
+        'img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img/4_enemie_boss_chicken/3_attack/G15.png',
+        'img/4_enemie_boss_chicken/3_attack/G16.png',
+        'img/4_enemie_boss_chicken/3_attack/G17.png',
+        'img/4_enemie_boss_chicken/3_attack/G18.png',
+        'img/4_enemie_boss_chicken/3_attack/G19.png',
+        'img/4_enemie_boss_chicken/3_attack/G20.png',
+    ];
+    IMAGES_HURT = [
+        'img/4_enemie_boss_chicken/4_hurt/G21.png',
+        'img/4_enemie_boss_chicken/4_hurt/G22.png',
+        'img/4_enemie_boss_chicken/4_hurt/G23.png',
+    ];
+    IMAGES_DEAD = [
+        'img/4_enemie_boss_chicken/5_dead/G24.png',
+        'img/4_enemie_boss_chicken/5_dead/G25.png',
+        'img/4_enemie_boss_chicken/5_dead/G26.png',
+    ];
 
     constructor() {
         super().loadImage('img/4_enemie_boss_chicken/2_alert/G5.png');
-        this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_ALERT);
+        //this.loadImages(this.IMAGES_WALK);
+        //this.loadImages(this.IMAGES_ATTACK); 
+        //this.loadImages(this.IMAGES_HURT); 
+        //this.loadImages(this.IIMAGES_DEAD);  
         //this.x = 2575;
         this.x = 400;
         this.animate();
     }
+
     animate() {
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING)
+            this.playAnimation(this.IMAGES_ALERT)
         }, 200);
     }
 
