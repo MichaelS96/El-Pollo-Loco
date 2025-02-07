b993cf12616bdfb225211ef40f2de8f0a9dc6e18
MichaelS96 <m.schwittay96@web.de>
2025-02-07 19:27:47 +0100

    jsdoc in throwable added

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,5 +1,12 @@
+/**
+ * Represents a throwable object in the game, such as a salsa bottle.
+ * Extends the MovableObject class.
+ */
 class ThrowableObject extends MovableObject {
+    /** @type {boolean} Indicates whether the object is colliding. */
     isColliding = false;
+
+    /** @type {Object} Defines the collision offset values. */
     offset = {
         top: 10,
         bottom: 10,
@@ -7,6 +14,7 @@ class ThrowableObject extends MovableObject {
         left: 10
     };
 
+    /** @type {string[]} Images for the bottle rotation animation. */
     IMAGES_ROTATION = [
         'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
         'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
@@ -14,6 +22,7 @@ class ThrowableObject extends MovableObject {
         'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
     ];
 
+    /** @type {string[]} Images for the bottle splash animation. */
     IMAGES_SPLASH = [
         'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
         'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
@@ -23,6 +32,11 @@ class ThrowableObject extends MovableObject {
         'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
     ];
 
+    /**
+     * Creates an instance of ThrowableObject.
+     * @param {number} x - The initial x-position of the object.
+     * @param {number} y - The initial y-position of the object.
+     */
     constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
         this.loadImages(this.IMAGES_ROTATION);
@@ -34,10 +48,13 @@ class ThrowableObject extends MovableObject {
         this.speedY = 30;
         this.splashSound = new Audio('audio/broken_bottle.mp3');
         soundManager.addSoundWithVolume(this.splashSound, 0.2);
-
+        
         this.throw();
     }
 
+    /**
+     * Initiates the throwing movement and animations.
+     */
     throw() {
         if (!this.throwStarted) {
             this.throwStarted = true;
@@ -53,6 +70,9 @@ class ThrowableObject extends MovableObject {
         }
     }
 
+    /**
+     * Animates the object, switching between rotation and splash animations.
+     */
     animate() {
         if (this.isAboveGround() && !this.isColliding) {
             this.playAnimation(this.IMAGES_ROTATION);
@@ -69,6 +89,9 @@ class ThrowableObject extends MovableObject {
         }
     }
 
+    /**
+     * Applies gravity to the object, making it fall.
+     */
     applyGravity() {
         this.gravityInterval = setInterval(() => {
             if (this.isAboveGround() || this.speedY > 0) {
